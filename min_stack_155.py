11c3208ce1e62f28e1359ab918b635a1330d77b9
gopiN2004 <gopisankarng@gmail.com>
2025-08-11 20:17:20 +0530

    leetcode_155

---
+++ min_stack_155.py
@@ -0,0 +1,16 @@
+class Minstack():
+    def __init__(self):
+        self.stack=[]
+        self.min_stack=[float("inf")]
+    def push(self,val):
+        self.stack.append(val)
+        self.min_stack.append(min(val,self.min_Stack[-1]))
+    def pop(self):
+        self.stack.pop()
+        self.min_Stack.pop()
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.min_stack[-1]
+
